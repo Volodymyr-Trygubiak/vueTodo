5638590c1e58ba0947d35a0a4c24ece641ed3e39
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-02-24 15:53:32 +0200

    created main component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="container">
+      <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <div class="header">
+      <input
+        class="main-inp"
+        @input="searchText = $event.target.value"
+        placeholder="Search..."
+        type="text"
+      />
+      <button class="main-btn" @click="hidden = false">Add new todo</button>
+    </div>
+
+    <AddNewTodo :hidden="hidden" @closeModal="closeModal" />
+
+    <TodoList :filteredTasks="filteredTasks" @deleteItem="deleteItem" />
   </div>
 </template>
 
 <script>
+import AddNewTodo from "./components/AddNewTodo";
+import TodoList from "./components/TodoList";
 
 export default {
   name: "App",
   components: {
-    
+    AddNewTodo,
+    TodoList
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Vue",
+          text: "Learn vue",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Angular",
+          text: "Learn Angular",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Html",
+          text: "Learn html",
+          completed: false
+        },
+        {
+          id: 4,
+          title: "scss",
+          text: "Learn scss",
+          completed: false
+        },
+        {
+          id: 5,
+          title: "Vuex",
+          text: "Learn Vuex",
+          completed: false
+        },
+        {
+          id: 6,
+          title: "React",
+          text: "Learn react",
+          completed: false
+        }
+      ],
+      searchText: "",
+      hidden: true
+    };
+  },
+  computed: {
+    filteredTasks() {
+      return this.tasks.filter(task =>
+        task.title.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    }
+  },
+  methods: {
+    closeModal() {
+      return (this.hidden = true);
+    },
+    deleteItem(index) {
+      this.tasks.splice(index, 1);
+    }
   }
 };
 </script>
@@ -20,7 +97,54 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 80px;
+  margin: 0 auto;
+  position: relative;
+}
+.container {
+  padding-bottom: 20px;
+  background-image: url(./assets/4k-ultra-hd-background-nebula-sparkles-shine-dark-abstractio.jpg);
+}
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 800px;
+  margin: 20px auto;
+  .main-inp {
+    margin-bottom: 0;
+  }
+  .main-btn {
+    white-space: nowrap;
+    margin-left: 20px;
+  }
+}
+
+.title {
+  display: inline-block;
+  font-size: 36px;
+  margin: 20px 0;
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: #41b883;
+  }
+}
+
+.title-inp {
+  font-size: 20px;
+  margin-top: 15px;
+  margin-bottom: 10px;
+  text-align: left;
 }
 </style>
