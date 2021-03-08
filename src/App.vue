235d3baccb23d7090842991ffb79f588edf9d632
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-03-08 11:41:16 +0200

    added vuex, axios and json server

--- src/App.vue
+++ src/App.vue
@@ -1,130 +1,122 @@
 <template>
   <div id="app">
-    <div class="container">
+    <div class="wrapper">
       <img class="logo" alt="Vue logo" src="./assets/logo.png" />
     </div>
-    <div class="header">
-      <input
-        class="main-inp"
-        @input="searchText = $event.target.value"
-        placeholder="Search..."
-        type="text"
+
+    <div class="container">
+      <div class="header">
+        <input
+          class="main-inp"
+          placeholder="Search..."
+          type="text"
+          v-model="searchText"
+        />
+        <TodoButton :title="'Add new todo'" @click="openCreateModal" />
+      </div>
+
+      <TodoCreateItem
+        :tasks="tasks"
+        :hidden="hidden"
+        @close-create-modal="closeCreateModal"
+        @check-form="checkForm"
       />
-      <button class="main-btn" @click="hidden = false">Add new todo</button>
+
+      <TodoList
+        :filtered-tasks="filteredTasks"
+        @open-delete-modal="openDeleteModal"
+        @check-item="checkItem"
+      />
+
+      <TodoDeteleItem
+        :filtered-tasks="filteredTasks"
+        :task-index="taskIndex"
+        :hidden-delete="hiddenDelete"
+        @close-delete-modal="closeDeleteModal"
+        @delete-item="deleteItem(taskIndex)"
+      />
     </div>
-
-    <AddNewTodo
-      :tasks="tasks"
-      :hidden="hidden"
-      @closeModal="closeModal"
-      @checkForm="checkForm"
-    />
-
-    <TodoList
-      :filteredTasks="filteredTasks"
-      @openDeleteModal="openDeleteModal"
-      @checkItem="checkItem"
-    />
-    <DeleteListItem
-      :tasks="tasks"
-      :hiddenDelete="hiddenDelete"
-      @closeDeleteModal="closeDeleteModal"
-      @deleteItem="deleteItem"
-    />
   </div>
 </template>
 
 <script>
-import AddNewTodo from "./components/AddNewTodo";
-import TodoList from "./components/TodoList";
-import DeleteListItem from "./components/DeteleListItem";
+import TodoButton from "./components/shared/TodoButton";
 
-import CloseModal from "./mixins/closeModal";
+import TodoCreateItem from "./components/TodoCreateItem";
+import TodoList from "./components/TodoList";
+import TodoDeteleItem from "./components/TodoDeteleItem";
+
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "App",
+
   components: {
-    AddNewTodo,
+    TodoButton,
+    TodoCreateItem,
     TodoList,
-    DeleteListItem
+    TodoDeteleItem
   },
-  mixins: [CloseModal],
+
   data() {
     return {
-      tasks: [
-        {
-          id: 1,
-          title: "Vue",
-          text: "Learn vue",
-          completed: true,
-        },
-        {
-          id: 2,
-          title: "Angular",
-          text: "Learn Angular",
-          completed: false,
-        },
-        {
-          id: 3,
-          title: "Html",
-          text: "Learn html",
-          completed: true,
-        },
-        {
-          id: 4,
-          title: "Scss",
-          text: "Learn scss",
-          completed: true,
-        },
-        {
-          id: 5,
-          title: "Vuex",
-          text: "Learn Vuex",
-          completed: false,
-        },
-        {
-          id: 5,
-          title: "Ham",
-          text: "Learn Vuex",
-          completed: false,
-        },
-        {
-          id: 6,
-          title: "React",
-          text: "Learn react",
-          completed: false,
-        },
-      ],
       searchText: "",
+      taskIndex: 0,
       hidden: true,
       hiddenDelete: true
     };
   },
+
   computed: {
+    ...mapGetters({
+      tasks: "TASKS"
+    }),
+
     filteredTasks() {
       return this.tasks.filter(task =>
         task.title.toLowerCase().includes(this.searchText.toLowerCase())
       );
     }
   },
+
+  mounted() {
+    this.GET_TASKS_FROM_API();
+  },
+
   methods: {
-    closeModal() {
+    ...mapActions(["GET_TASKS_FROM_API"]),
+
+    openCreateModal() {
+      this.hidden = false;
+      this.searchText = "";
+    },
+
+    closeCreateModal() {
       return (this.hidden = true);
     },
+
     checkForm(newTask) {
       return this.tasks.push(newTask);
     },
-    openDeleteModal() {
+
+    openDeleteModal(index) {
+      this.taskIndex = index;
+      console.log(index);
       return (this.hiddenDelete = false);
     },
+
     closeDeleteModal() {
       return (this.hiddenDelete = true);
     },
+
     deleteItem(index) {
+      this.taskIndex = 0;
       this.tasks.splice(index, 1);
     },
+
     checkItem(index) {
-      // this.$set(this.tasks[index], "completed", !this.tasks[index].completed);
-      this.tasks[index].completed = !this.tasks[index].completed;
+      this.filteredTasks[index].completed = !this.filteredTasks[index]
+        .completed;
     }
   }
 };
@@ -140,7 +132,7 @@ export default {
   margin: 0 auto;
   position: relative;
 }
-.container {
+.wrapper {
   padding-bottom: 20px;
   background-image: url(./assets/4k-ultra-hd-background-nebula-sparkles-shine-dark-abstractio.jpg);
 }
@@ -157,33 +149,11 @@ export default {
   margin: 20px auto;
   .main-inp {
     margin-bottom: 0;
+    background-color: #111111;
   }
-  .main-btn {
+  .btn {
     white-space: nowrap;
     margin-left: 20px;
   }
-}
-
-.title {
-  display: inline-block;
-  font-size: 36px;
-  margin: 20px 0;
-  position: relative;
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background-color: #41b883;
-  }
-}
-
-.title-inp {
-  font-size: 20px;
-  margin-top: 15px;
-  margin-bottom: 10px;
-  text-align: left;
 }
 </style>
