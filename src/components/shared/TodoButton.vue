da4484cc5399e48b70a9495db068a7f6d1356088
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-03-08 11:41:53 +0200

    added shared components

---
+++ src/components/shared/TodoButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="btn" :class="className" @click="click">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TodoButton",
+
+  props: {
+    title: {
+      type: String,
+      default: "Press"
+    },
+    cancel: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    className() {
+      return {
+        cancel: this.cancel
+      };
+    }
+  },
+  methods: {
+    click() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/shared/TodoModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="modal-overlay" @click.self="closeModal">
+    <div class="modal-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoModal",
+
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    }
+  }
+};
+</script>
